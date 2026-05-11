c2981bac35076a53ec6c5206e257c32c6520899c
Absolutely CraiCrai <jempcorp@ymail.com>
2026-05-11 20:01:29 +0100

    Version string

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -53,7 +53,7 @@ namespace OpenSim
 
         public static string GetVersionString(string versionNumber, Flavour flavour)
         {
-            string versionString = $"OpenSim {versionNumber} Nessie {flavour}";
+            string versionString = $"InWorld Grid Server (OSS {versionNumber} {flavour})";
             return versionString.PadRight(VERSIONINFO_VERSION_LENGTH);
         }
 
